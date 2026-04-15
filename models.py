72a07ba1cbbc482dc2ce95dcbd7afe491d074b84
devarifkhan <arifcse209@gmail.com>
2026-04-15 11:51:47 +0600

    Add Todos SQLAlchemy model with owner foreign key

--- models.py
+++ models.py
@@ -14,3 +14,14 @@ class Users(Base):
     is_active = Column(Boolean, default=True)
     role = Column(String)
     phone_number = Column(String)
+
+
+class Todos(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
